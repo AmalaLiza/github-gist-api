1c90d4f6a94fb5c25e836afbd2287238c55c734d
AmalaLiza <amala.james@razorthink.com>
2017-10-24 23:11:06 +0530

    Revert "Revert "Revert "Initial commit"""
    This reverts commit 525a70ba257c132bf4fc22bd410c141eeff7227f.

--- src/saga.js
+++ src/saga.js
@@ -1,9 +1,11 @@
-import signIn from './sagas/signIn.saga';
+import loadPublicGistsOfUser from './sagas/loadPublicGistsOfUser.saga';
+import loadForks from './sagas/loadForks.saga';
 
 /**
  * Exporting all sagas used for the applications.
  * **/
 
 export default [
-  signIn,
+  loadPublicGistsOfUser,
+  loadForks
 ];
