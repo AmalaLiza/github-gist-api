8ced4b4a055cff6dac71070ab95a6c7d59a7d488
AmalaLiza <amala.james@razorthink.com>
2017-10-24 22:56:38 +0530

    Initial commit

--- src/components/PublicGists/gists.selector.js
+++
@@ -1,9 +0,0 @@
-
-export const selectGists = state => ({
-  gists: state.gistsApi.get('publicGists'),
-  user: state.gistsApi.get('user'),
-});
-
-export const getError = state => ({
-  error: state.gistsApi.get('error'),
-});
